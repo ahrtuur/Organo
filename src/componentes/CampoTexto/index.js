8d1ebc49cc068d55fc4d11294d32f46d9358248a
ahrtuur <arthurcontrig@gmail.com>
2026-06-28 21:31:35 -0300

    quinto commit

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,13 @@
 import "./CampoTexto.css"
 
-const CampoTexto = () => { 
+const CampoTexto = (props) => { 
+    console.log(props.label)
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder="Digite o seu nome"/>
+            <label>
+                {props.log}
+            </label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 }
