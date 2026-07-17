bba8cc9221ce7683f6ddb214ed3af90896e7bba1
ahrtuur <arthurcontrig@gmail.com>
2026-07-17 19:17:02 -0300

    decimo primeiro commit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import "./Formulario.css"
 
 const Formulario = (props) => {
 
-    const times = [
-        "Programação",
-        "Front-end",
-        "Data Science",
-        "Devops",
-        "UX e Design",
-        "Mobile",
-        "Inovação e Gestão"
-    ]
-
     const [nome, setNome] = useState("")
     const [cargo, setCargo] = useState("")
     const [imagem, setImagem] = useState("")
@@ -59,7 +49,8 @@ const Formulario = (props) => {
                 />
                 <ListaSuspensa 
                     obrigatorio = {true} 
-                    label="Time" itens={times}
+                    label="Time" 
+                    itens={props.times}
                     valor={time}
                     aoAlterado ={valor => setTime(valor)}
                 />
