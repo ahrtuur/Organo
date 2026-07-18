b5a2b10294a2924a6744e854ac9a60e2f5cbbb2a
ahrtuur <arthurcontrig@gmail.com>
2026-07-18 14:51:10 -0300

    12 commit

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => { 
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/ahrtuur.png' alt='Arthur'/>
+        </div>
+        <div className='rodape'>
+            <h4>Arthur Contri</h4>
+            <h5>Desenvolvedor Front-end</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
+
